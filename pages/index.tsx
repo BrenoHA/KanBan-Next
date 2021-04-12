bf313f56fc24476a0ef130a3be56f590cf345c6a
BrenoHA <breno.ha@gmail.com>
2021-04-12 19:29:57 -0300

    Add react-dnd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import { ReactElement } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import Header from '@app/components/Header';
 import Board from '@app/components/Board';
 
 const HomePage = (): ReactElement => {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Header />
       <Board />
-    </>
+    </DndProvider>
   );
 };
 
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,22 +1,46 @@
 import { ICard } from '@app/interfaces/tarefa';
 import style from './style.module.css';
+import { useDrag, useDrop } from 'react-dnd';
 
 type card = {
   data: ICard;
 };
 
 const Card = ({ data }: card) => {
+  const [{ isDragging }, dragRef] = useDrag(() => ({
+    type: 'CARD',
+    // item: { type: 'CARD' },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+
   return (
-    <div className={style.container}>
-      <header>
-        <span className={style.label}></span>
-        {/* #TODO change label colors */}
-      </header>
-      <p>{data.content}</p>
-      {data.user && <img src={data.user} alt="avatar" />}
-      {/* #TODO change avatars */}
-    </div>
+    <>
+      {isDragging ? (
+        <div
+          className={`${style.container} ${style.containerIsDragging}`}
+          ref={dragRef}
+        >
+          <header>
+            <span className={style.label}></span>
+          </header>
+          <p>{data.content}</p>
+          {data.user && <img src={data.user} alt="avatar" />}
+        </div>
+      ) : (
+        <div className={style.container} ref={dragRef}>
+          <header>
+            <span className={style.label}></span>
+          </header>
+          <p>{data.content}</p>
+          {data.user && <img src={data.user} alt="avatar" />}
+        </div>
+      )}
+    </>
   );
 };
 
+// TODO change label colors */
+// TODO change avatars */
 export default Card;
